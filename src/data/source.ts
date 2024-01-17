e9c6a64f9673e7c2467bcb7d8ffbfa2b5ddf38fa
Geraldo Silva <geraldojuni@hotmail.com>
2024-01-17 19:52:25 -0300

    chore: create basic data source file

---
+++ src/data/source.ts
@@ -0,0 +1,52 @@
+export const cards = [
+  {
+    front: 'What is the rotation interval for AWS-managed KMS keys?',
+    back: '1 year',
+  },
+  {
+    front: 'What AWS service can be used to audit a KMS Key usage?',
+    back: 'CloudTrail',
+  },
+  {
+    front: 'What is the maximum execution duration for an Express Step Function?',
+    back: '5 minutes',
+  },
+  {
+    front: 'What infrastructure creation tool is utilized by AWS SAM?',
+    back: 'CloudFormation',
+  },
+  {
+    front: 'What is the maximum execution duration for a Standard Step Function?',
+    back: '1 year',
+  },
+  {
+    front: 'Where are AWS CodeBuild artifacts stored?',
+    back: 'S3',
+  },
+  {
+    front: 'How can you enable different origins from making requests to your API Gateway?',
+    back: 'Enable CORS',
+  },
+  {
+    front: 'What property can you use to get only specified fields from a DynamoDB query?',
+    back: 'ProjectionExpression',
+  },
+  {
+    front: 'What is the maximum execution duration for a Lambda function?',
+    back: '15 minutes',
+  },
+  {
+    front: 'What is the simplest way to delete old items from a DynamoDB table?',
+    back: 'Set a TTL',
+  },
+  {
+    front: 'What is the default concurrency limit for Lambda functions in an AWS account for the same region?',
+    back: '1000',
+  },
+  {
+    front: `
+What is the maximum number of concurrent executions that can be reserved for all Lambda functions in a region + account combination 
+using the default concurrency limit?`,
+    back: '900',
+  },
+];
