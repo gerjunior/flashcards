b112b841dca07d621db6a07ade31d1be14ec4444
Geraldo Silva <geraldojuni@hotmail.com>
2024-01-17 22:52:58 -0300

    feat: add colors on cards based on categories

--- src/components/CardFlip/CardFlip.tsx
+++ src/components/CardFlip/CardFlip.tsx
@@ -1,6 +1,12 @@
 import './CardFlip.css';
 import { Card } from '../../data/source';
 
+const colorMap: Record<Card['category'], string> = {
+  database: 'bg-blue-800',
+  development: 'bg-green-800',
+  security: 'bg-yellow-800',
+};
+
 type CardFlipProps = {
   card: Card;
   shouldAnimate?: boolean;
@@ -12,10 +18,14 @@ export const CardFlip = ({ card, shouldAnimate, handleClick, isFlipped }: CardFl
   return (
     <div className="w-96 h-56 m-4 scale-150">
       <div className={`card ${shouldAnimate ? 'duration-700 ease-in-out' : ''} ${isFlipped ? 'rotate-y-180' : ''}`} onClick={handleClick}>
-        <div className="front absolute w-full h-full bg-slate-800 rounded shadow-lg flex items-center justify-center p-10">
+        <div className={`front absolute w-full h-full ${colorMap[card.category]} rounded shadow-lg flex items-center justify-center p-10`}>
           {card.front}
         </div>
-        <div className="back absolute w-full h-full bg-slate-800 rounded shadow-lg flex items-center justify-center rotate-y-180">
+        <div
+          className={`back absolute w-full h-full ${
+            colorMap[card.category]
+          } rounded shadow-lg flex items-center justify-center rotate-y-180`}
+        >
           {card.back}
         </div>
       </div>
--- src/data/source.ts
+++ src/data/source.ts
@@ -1,57 +1,70 @@
 export type Card = {
   front: string;
   back: string;
+  category: 'development' | 'security' | 'database';
 };
 
 export const cards: Card[] = [
   {
     front: 'What is the rotation interval for AWS-managed KMS keys?',
     back: '1 year',
+    category: 'security',
   },
   {
     front: 'What AWS service can be used to audit a KMS Key usage?',
     back: 'CloudTrail',
+    category: 'security',
   },
   {
     front: 'What is the maximum execution duration for an Express Step Function?',
     back: '5 minutes',
+    category: 'development',
   },
   {
     front: 'What infrastructure creation tool is utilized by AWS SAM?',
     back: 'CloudFormation',
+    category: 'development',
   },
   {
     front: 'What is the maximum execution duration for a Standard Step Function?',
     back: '1 year',
+    category: 'development',
   },
   {
     front: 'Where are AWS CodeBuild artifacts stored?',
     back: 'S3',
+    category: 'development',
   },
   {
     front: 'How can you enable different origins from making requests to your API Gateway?',
     back: 'Enable CORS',
+    category: 'development',
   },
   {
     front: 'What property can you use to get only specified fields from a DynamoDB query?',
     back: 'ProjectionExpression',
+    category: 'database',
   },
   {
     front: 'What is the maximum execution duration for a Lambda function?',
     back: '15 minutes',
+    category: 'development',
   },
   {
     front: 'What is the simplest way to delete old items from a DynamoDB table?',
     back: 'Set a TTL',
+    category: 'database',
   },
   {
     front: 'What is the default concurrency limit for Lambda functions in an AWS account for the same region?',
     back: '1000',
+    category: 'development',
   },
   {
     front: `
 What is the maximum number of concurrent executions that can be reserved for all Lambda functions in a region + account combination 
 using the default concurrency limit?`,
     back: '900',
+    category: 'development',
   },
 ];
